c279f402134165c6cf47f21bb3c7cbd9c449f4c5
jayaram <jayramksm@gmail.com>
2020-02-20 17:48:06 +0530

    20\02\2020

---
+++ array.ts
@@ -0,0 +1,12 @@
+var arr=[10,"jay",30,100,"ram"];
+console.log(arr[4]);
+
+var ar:number[]=[20,30,40,80,90]
+console.log(ar[3]);
+
+var arra:(number|string)[]=[20,"jayaram",40,52,"hello"]
+console.log(arra[2]);
+
+type unic=(number|string)[];
+var array:unic=[20,"hello",525,"hii"];
+console.log(array[1])
---
+++ object.ts
@@ -0,0 +1,29 @@
+var obj:{
+    id:number;
+    fname:string;
+    lname:string;
+    age:number;
+    // getname:string
+
+}={
+    id:101,
+    fname:"jayaram",
+    lname:"kasim",
+    age:22,
+
+}
+console.log(obj.fname);
+
+
+var person = {
+    firstname: "Tom",
+    lastname: "Hanks"
+ };
+ 
+ var invokeperson = function (obj) {
+    console.log("first name :" + obj.firstname);
+    console.log("last name :" + obj.lastname);
+ };
+ invokeperson(person);
+
+ 
